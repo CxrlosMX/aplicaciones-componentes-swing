3b7745c6fd1aaa2413c2cbfb32a993df2bf39359
CxrlosMX <LuisCRendon131@gmail.com>
2021-09-08 18:09:09 -0500

    Aplicacion registro v1 terminada

--- src/registropeliculas/LaminaPeliculas.java
+++ src/registropeliculas/LaminaPeliculas.java
@@ -7,18 +7,18 @@ package registropeliculas;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
+
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
+
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
